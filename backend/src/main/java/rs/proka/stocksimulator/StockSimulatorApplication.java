c6d7353e6a5cadd2f1b495ec450bc65c87369d8e
Dejan Prokić <dejanp@nordeus.com>
2023-11-13 18:46:48 +0100

    Initialize Spring Boot

---
+++ backend/src/main/java/rs/proka/stocksimulator/StockSimulatorApplication.java
@@ -0,0 +1,13 @@
+package rs.proka.stocksimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSimulatorApplication.class, args);
+	}
+
+}
